1f9485e73f00f6a98581c6bca8b8f59066e87d85
Roman Reznikov <romanreznikov2002@yandex.ru>
2024-09-02 00:10:29 -0400

    fix: use pagination class compatible with JSON:API

--- b2broker/api/pagination.py
+++ b2broker/api/pagination.py
@@ -1,5 +1,7 @@
-from rest_framework_json_api.pagination import PageNumberPagination
+from rest_framework_json_api.pagination import JsonApiPageNumberPagination
+
+from django.conf import settings
 
 
-class SmallResultsSetPagination(PageNumberPagination):  # type: ignore[misc]
-    page_size = 10
+class SmallResultsSetPagination(JsonApiPageNumberPagination):  # type: ignore[misc]
+    page_size = settings.DEFAULT_SMALL_PAGE_SIZE
